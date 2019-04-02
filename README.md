aadf7b2622c97a54b70a19ecdfbef9c85b8b3b39
Alex Frasson <afrasson@inf.ufsm.br>
2019-04-02 23:33:33 +0200

    Add AppVeyor yml file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ChromaRenderer
+
+[![Build status](https://ci.appveyor.com/api/projects/status/nio750gu5ia85v48?svg=true)](https://ci.appveyor.com/project/alexfrasson/chromarenderer)
---
+++ build.cmd
@@ -0,0 +1,27 @@
+@echo off
+echo Downloading CUDA toolkit 10.0
+appveyor DownloadFile https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_411.31_win10 -FileName cuda_10.0.130_411.31_win10.exe
+echo Installing CUDA toolkit 10.0
+cuda_10.0.130_411.31_win10.exe -s nvcc_10.0 ^
+                                  curand_10.0 ^
+                                  curand_dev_10.0 ^
+                                  cudart_10.0
+
+::dir "%ProgramFiles%"
+::dir "C:\Program Files"
+::dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
+::dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0"
+::dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin"
+
+if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin\cudart64_100.dll" ( 
+echo "Failed to install CUDA"
+exit /B 1
+)
+
+set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.0\libnvvp;%PATH%
+
+nvcc -V
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+msbuild "C:\projects\chromarenderer\ChromaRenderer\ChromaRenderer.sln" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
