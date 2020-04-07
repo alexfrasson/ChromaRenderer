65356204e8c7dd9bc788abcd52fdf747e63fc16d
Alex Frasson <afrasson@inf.ufsm.br>
2020-04-07 19:06:57 +0200

    Build bazel project

--- build.cmd
+++ build.cmd
@@ -1,6 +1,10 @@
 @echo off
+
+echo Current directory: %cd%
+
 echo Downloading CUDA toolkit 10.0
 appveyor DownloadFile https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_411.31_win10 -FileName cuda_10.0.130_411.31_win10.exe
+
 echo Installing CUDA toolkit 10.0
 cuda_10.0.130_411.31_win10.exe -s nvcc_10.0 ^
                                   curand_10.0 ^
@@ -22,6 +26,13 @@ set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin;%ProgramFile
 
 nvcc -V
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+echo Downloading bazelisk
+appveyor DownloadFile https://github.com/bazelbuild/bazelisk/releases/download/v1.3.0/bazelisk-windows-amd64.exe -FileName bazelisk.exe
 
-msbuild "C:\projects\chromarenderer\ChromaRenderer\ChromaRenderer.sln" /verbosity:detailed /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+cd "C:\projects\chromarenderer\ChromaRenderer"
+
+"C:\projects\chromarenderer\bazelisk.exe" build //ChromaRenderer:chroma-renderer
+
+::"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+::msbuild "C:\projects\chromarenderer\ChromaRenderer\ChromaRenderer.sln" /verbosity:detailed /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
